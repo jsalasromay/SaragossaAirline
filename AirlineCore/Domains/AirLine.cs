5a519e3d1d506bcd4d4672b404806b092bd3b5d8
Jesus <jsalas@nettrim.es>
2021-10-07 23:49:31 +0200

    Add project files.

---
+++ AirlineCore/Domains/AirLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AirlineCore.Domains
+{
+    public class AirLine
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(10, MinimumLength = 3)]
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+        [Range(100, 100000)]
+        public decimal AnnualAverage { get; set; }
+    }
+}
---
+++ AirlineCore/Domains/CheckIn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirlineCore.Domains
+{
+    public class CheckIn
+    {
+        public string LicensePlate { get; set; }
+        public string Dni { get; set; }
+        public DateTime FlyDate { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
